ab6193158903bcd118b2aa8d721c54af3c2b3f3c
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-06-22 10:05:03 +0100

    empty functions

---
+++ R/reportPhenotype.R
@@ -0,0 +1,12 @@
+
+#' Create a report summarising your phenotyping results
+#'
+#' @param result A summarised result
+#'
+#' @return A report
+#' @export
+#'
+#' @examples
+reportPhenotype <- function(result){
+
+}
---
+++ R/shinyPhenotype.R
@@ -0,0 +1,12 @@
+
+#' Create a shiny app summarising your phenotyping results
+#'
+#' @param result A summarised result
+#'
+#' @return A shiny app
+#' @export
+#'
+#' @examples
+shinyPhenotype <- function(result){
+
+}
