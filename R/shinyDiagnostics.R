dc92810a49a93e0d032d1ac6b8251e46b9e36adf
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-06-26 14:49:31 +0100

    update

--- R/shinyDiagnostics.R
+++ R/shinyDiagnostics.R
@@ -2,7 +2,6 @@
 #' Create a shiny app summarising your phenotyping results
 #'
 #' @param result A summarised result
-#' @param testMode testMode
 #'
 #' @return A shiny app
 #' @export
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -4,12 +4,62 @@ ui = dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem(
-        text = "Code counts",
-        tabName = "code_counts"
+        text = "Background",
+        tabName = "background"),
+      menuItem(
+        text = "Databases",
+        tabName = "cdm_summary"),
+      menuItem(
+        text = "CodelistDiagnostics",
+        tabName = "codelist_diagnostics",
+        menuSubItem(
+          text = "Code use",
+          tabName = "code_use"
+        ),
+        menuSubItem(
+          text = "Index events",
+          tabName = "index_events"
+        ),
+        menuSubItem(
+          text = "Orphan concepts",
+          tabName = "orphan_concepts"
+        )
       ),
       menuItem(
-        text = "Cohort overlap",
-        tabName = "overlap"
+        text = "Cohort diagnostics",
+        tabName = "cohort_diagnostics",
+        menuSubItem(
+          text = "Cohort counts",
+          tabName = "cohort_counts"
+        ),
+        menuSubItem(
+          text = "Cohort attrition",
+          tabName = "cohort_attrition"
+        ),
+        menuSubItem(
+          text = "Cohort overlap",
+          tabName = "cohort_overlap"
+        ),
+        menuSubItem(
+          text = "Cohort timinig",
+          tabName = "cohort_timining"
+        ),
+        menuSubItem(
+          text = "Incidence",
+          tabName = "incidence"
+        ),
+        menuSubItem(
+          text = "Prevalence",
+          tabName = "prevalence"
+        ),
+        menuSubItem(
+          text = "Cohort characteristics",
+          tabName = "cohort_characteristics"
+        ),
+        menuSubItem(
+          text = "Large scale characteristics",
+          tabName = "lsc"
+        )
       )
     )
   ),
