57aee116a89bc0a91f9f3a261daac887ef72b461
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-06-26 13:18:19 +0100

    setup shiny

--- R/shinyDiagnostics.R
+++ R/shinyDiagnostics.R
@@ -2,11 +2,18 @@
 #' Create a shiny app summarising your phenotyping results
 #'
 #' @param result A summarised result
+#' @param testMode testMode
 #'
 #' @return A shiny app
 #' @export
 #'
 #' @examples
 shinyDiagnostics <- function(result){
+
+  appDir <- system.file("shiny",
+                        "app.R",
+                        package = "phenotypeR")
+
+  shiny::shinyAppFile(appDir)
 
 }
