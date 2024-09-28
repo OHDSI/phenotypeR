2c3e81c849aba7c589b51dab7a7932c43e85134d
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-09-28 11:52:23 +0100

    database diagnostics

---
+++ tests/testthat/test-databaseDiagnostics.R
@@ -0,0 +1,20 @@
+test_that("multiplication works", {
+  cdm_local <- omock::mockCdmReference() |>
+    omock::mockPerson(nPerson = 100) |>
+    omock::mockObservationPeriod() |>
+    omock::mockConditionOccurrence() |>
+    omock::mockDrugExposure() |>
+    omock::mockObservation() |>
+    omock::mockMeasurement() |>
+    omock::mockCohort(name = "my_cohort", numberCohorts = 2)
+
+  db <- DBI::dbConnect(duckdb::duckdb())
+  cdm <- CDMConnector::copyCdmTo(con = db, cdm = cdm_local,
+                                 schema ="main", overwrite = TRUE)
+
+  db_diag <- databaseDiagnostics(cdm)
+  expect_no_error(OmopSketch::tableOmopSnapshot(db_diag))
+
+  CDMConnector::cdmDisconnect(cdm = cdm)
+
+})
