a75837988dc2073f9176ff6d5df32c782d405efb
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-09-29 21:39:52 +0100

    add funs to reexport

---
+++ R/reexports.R
@@ -0,0 +1,35 @@
+#' @importFrom omopgenerics suppress
+#' @export
+omopgenerics::suppress
+
+#' @importFrom omopgenerics settings
+#' @export
+omopgenerics::settings
+
+#' @importFrom omopgenerics bind
+#' @export
+omopgenerics::bind
+
+#' @importFrom omopgenerics importSummarisedResult
+#' @export
+omopgenerics::importSummarisedResult
+
+#' @importFrom omopgenerics exportSummarisedResult
+#' @export
+omopgenerics::exportSummarisedResult
+
+#' @importFrom CodelistGenerator summariseAchillesCodeUse
+#' @export
+CodelistGenerator::summariseAchillesCodeUse
+
+#' @importFrom CodelistGenerator summariseCohortCodeUse
+#' @export
+CodelistGenerator::summariseCohortCodeUse
+
+#' @importFrom CodelistGenerator summariseCodeUse
+#' @export
+CodelistGenerator::summariseCodeUse
+
+#' @importFrom CodelistGenerator summariseOrphanCodes
+#' @export
+CodelistGenerator::summariseOrphanCodes
