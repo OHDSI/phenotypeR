c272d1d0afa7235b50d3b4c7be5cfa8ab33a656e
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-09-30 11:53:18 +0100

    individual arguments

--- R/phenotype.R
+++ R/phenotype.R
@@ -2,6 +2,11 @@
 #' Phenotype a cohort
 #'
 #' @param cohort Cohort
+#' @param databaseDiagnostics If TRUE, database diagnostics will be run.
+#' @param codelistDiagnostics If TRUE, codelist diagnostics will be run.
+#' @param cohortDiagnostics If TRUE, cohort diagnostics will be run.
+#' @param cohortToPopulationDiagnostics If TRUE, cohort to population
+#' diagnostics will be run.
 #' @param nSample The number of people to take a random sample for matching to
 #' the database population. If NULL, no sampling will be performed and the
 #' entire cohorts will be used.
@@ -11,21 +16,36 @@
 #'
 #' @examples
 phenotype <- function(cohort,
+                      databaseDiagnostics = TRUE,
+                      codelistDiagnostics = TRUE,
+                      cohortDiagnostics = TRUE,
+                      cohortToPopulationDiagnostics = TRUE,
                       nSample = 1000){
 
   cdm <- omopgenerics::cdmReference(cohort)
 
   results <- list()
-
-  results[["db_diag"]] <- databaseDiagnostics(cdm)
+  if(isTRUE(databaseDiagnostics)){
+    results[["db_diag"]] <- databaseDiagnostics(cdm)
+  }
+  if(isTRUE(codelistDiagnostics)){
   results[["code_diag"]] <- codelistDiagnostics(cohort)
+  }
+  if(isTRUE(cohortDiagnostics)){
   results[["cohort_diag"]] <- cohortDiagnostics(cohort)
+  }
+  if(isTRUE(cohortToPopulationDiagnostics)){
   results[["cohort_to_pop_diag"]] <- cohortToPopulationDiagnostics(cohort,
                                                                    nSample = nSample)
+  }
 
   results <- results |>
     vctrs::list_drop_empty() |>
     omopgenerics::bind()
+
+  if(is.null(results)){
+    results <- omopgenerics::emptySummarisedResult()
+  }
 
   results
 
