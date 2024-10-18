2b15ee446cb125785d75e19b97dca4a936028bbf
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-10-18 12:43:23 +0100

    adding descriptions to each exported function

--- R/phenotypeDiagnostics.R
+++ R/phenotypeDiagnostics.R
@@ -1,4 +1,3 @@
-
 #' Phenotype a cohort
 #'
 #' @inheritParams cohortDoc
@@ -15,6 +14,38 @@
 #' @export
 #'
 #' @examples
+#' \donttest{
+#'   cdm_local <- omock::mockCdmReference() |>
+#'     omock::mockPerson(nPerson = 100) |>
+#'     omock::mockObservationPeriod() |>
+#'     omock::mockConditionOccurrence() |>
+#'     omock::mockDrugExposure() |>
+#'     omock::mockObservation() |>
+#'     omock::mockMeasurement() |>
+#'     omock::mockCohort(name = "my_cohort",
+#'                       numberCohorts = 2)
+#'   cdm_local$visit_occurrence <- dplyr::tibble(
+#'     person_id = 1L,
+#'     visit_occurrence_id = 1L,
+#'     visit_concept_id = 1L,
+#'     visit_start_date = as.Date("2000-01-01"),
+#'     visit_end_date = as.Date("2000-01-01"),
+#'     visit_type_concept_id = 1L)
+#'   cdm_local$procedure_occurrence <- dplyr::tibble(
+#'     person_id = 1L,
+#'     procedure_occurrence_id = 1L,
+#'     procedure_concept_id = 1L,
+#'     procedure_date = as.Date("2000-01-01"),
+#'     procedure_type_concept_id = 1L)
+#'
+#'   db <- DBI::dbConnect(duckdb::duckdb())
+#'   cdm <- CDMConnector::copyCdmTo(con = db,
+#'                                  cdm = cdm_local,
+#'                                  schema ="main",
+#'                                  overwrite = TRUE)
+#'   phenotypeDiagnostics(cdm$my_cohort)
+#'   CDMConnector::cdm_disconnect(cdm)
+#' }
 phenotypeDiagnostics <- function(cohort,
                                  databaseDiagnostics = TRUE,
                                  codelistDiagnostics = TRUE,
